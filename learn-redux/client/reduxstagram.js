8fed506ca082ffb508bc7cc0ca3095b3c96f9c9f
srzvan <sbingu.razvan@gmail.com>
2020-09-10 17:18:20 +0300

    created router

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
 
 import css from './styles/style.styl';
 
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid} />
+      <Route path="/view/:postId" component={Single} />
+    </Route>
+  </Router>
+);
+
 const mountPoint = document.getElementById('root');
-render(<Main />, mountPoint);
+render(router, mountPoint);
