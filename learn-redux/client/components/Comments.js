1173914b7edc00df83b07684a173068a461e2d83
srzvan <sbingu.razvan@gmail.com>
2020-09-11 16:51:23 +0300

    passed postComments from <Single /> to <Comments />, renderComment method & add comment form

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
 class Comments extends React.Component {
+  renderComment(comment, index) {
+    return (
+      <div key={index} className="comment">
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    );
+  }
+
   render() {
-    return <div className="comment">I am a acomment</div>;
+    return (
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="text" ref="authorRef" placeholder="Your name" />
+          <input type="text" ref="newCommentRef" placeholder="Comment..." />
+          <input type="submit" hidden />
+        </form>
+      </div>
+    );
   }
 }
 
