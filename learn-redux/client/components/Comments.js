b23bd1bb03af3725aaaf60b9e17ebbaa9cc98bd9
srzvan <sbingu.razvan@gmail.com>
2020-09-11 16:22:30 +0300

    render <Photo /> & (new) <Comments /> components inside of <Single />

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Comments extends React.Component {
+  render() {
+    return <div className="comment">I am a acomment</div>;
+  }
+}
+
+export default Comments;
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import Photo from './Photo';
+import Comments from './Comments';
+
 class Single extends React.Component {
   render() {
-    return <div className="single-photo">Single</div>;
+    const index = this.props.posts.findIndex(
+      (post) => post.code === this.props.params.postId
+    );
+    const post = this.props.posts[index];
+
+    return (
+      <div className="single-photo">
+        <Photo index={index} post={post} {...this.props} />
+        <Comments />
+      </div>
+    );
   }
 }
 
