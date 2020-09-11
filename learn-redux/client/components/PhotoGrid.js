a4f6d5e8a2b1300d0e92dcee321df753de55ceb8
srzvan <sbingu.razvan@gmail.com>
2020-09-11 15:57:31 +0300

    new <Photo /> component, render <Photo />s in <PhotoGrid />

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import Photo from './Photo';
+
 class PhotoGrid extends React.Component {
   render() {
-    return <div className="photo-grid">PhotoGrid</div>;
+    return (
+      <div className="photo-grid">
+        {this.props.posts.map((post, index) => (
+          <Photo key={index} index={index} post={post} {...this.props} />
+        ))}
+      </div>
+    );
   }
 }
 
